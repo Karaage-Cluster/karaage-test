95243b457f1d715761ed31e52b844329aaf501c5
Brian May <brian@microcomaustralia.com.au>
2014-01-23 14:27:29 +1100

    Add DATASTORES to karaage settings.

--- 389/global_settings.py
+++ 389/global_settings.py
@@ -85,6 +85,24 @@ LDAP = {
     }
 }
 
+DATASTORES = {
+    'ldap' : [
+        {
+            'DESCRIPTION': 'Default LDAP datastore',
+            'ENGINE': 'karaage.datastores.ldap.AccountDataStore',
+            'LDAP': 'default',
+            'ACCOUNT': 'karaage.datastores.ldap_schemas.openldap_account',
+            'GROUP': 'karaage.datastores.ldap_schemas.openldap_group',
+            'PRIMARY_GROUP': "institute",
+            'DEFAULT_PRIMARY_GROUP': "dummy",
+            'HOME_DIRECTORY': "/home/%(uid)s",
+            'LOCKED_SHELL': "/usr/local/sbin/locked",
+        },
+    ],
+    'dummy' : [
+    ],
+}
+
 ###
 ### Django PBS settings
 ###
