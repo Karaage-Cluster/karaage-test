58f1a06eeb27177aecc9cec216f701b46494d257
Brian May <brian@microcomaustralia.com.au>
2015-06-04 15:07:04 +1000

    Updates for Jessie.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -8,9 +8,9 @@ export DEBCONF_DEBUG=developer
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install --yes python-pip
-pip install schroot
-# apt-get install python-schroot python3-schroot
+# apt-get install --yes python-pip
+# pip install schroot
+apt-get install --yes python-schroot python3-schroot
 
 cd "$DIR"
-./dotest --distribution=wheezy  --ldap=openldap --localhost --keep
+./dotest --distribution=jessie  --ldap=openldap --localhost --keep --create_superuser
